688a03a9af4a8510377a5369ec923a439674bac5
Corentin <cordiercorentin01@gmail.com>
2026-02-12 22:10:49 +0100

    feat: Ajouter fichiers de configuration Make et améliorer .gitignore

---
+++ mk/dd.mk
@@ -0,0 +1,3 @@
+https://discord.com/oauth2/authorize?client_id=1467202135757291795 
+#pour la permission de bot uniquement
+&permissions=8
---
+++ mk/idéé.mk
@@ -0,0 +1,11 @@
+8. /stats 📊
+Statistiques personnelles (nombre de téléchargements)
+Vidéos les plus téléchargées
+Classement des utilisateurs les plus actifs
+9. /leaderboard 🏆
+Top 10 des utilisateurs les plus actifs
+Système de points/badges
+Récompenses mensuelles
+10. /suggest 💡
+Suggère du contenu tendance
+Basé sur les téléchargements populaires du serveur
